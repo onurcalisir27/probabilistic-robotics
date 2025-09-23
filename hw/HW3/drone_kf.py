488d33823a20b385732aa49dbd3cd57d44e89de6
Onur Calisir <onur.calisir@columbia.edu>
2025-09-22 22:12:23 -0400

    Homework 3 solutions

---
+++ hw/HW3/drone_kf.py
@@ -0,0 +1,99 @@
+import numpy
+import math
+
+default_dt = 1 / 200
+default_var = 0.25
+
+class DroneKF:
+    def __init__(self, mass, thrust):
+        self.m = mass
+        self.T = thrust
+        self.g = 9.81
+        self.state = numpy.zeros((2,1))
+        self.state_cov = numpy.identity(2) * default_var
+        self.predicted_state = numpy.zeros((2,1))
+        self.predicted_state_cov = numpy.identity(2) * default_var
+        self.time = None
+
+    def set_state(self, timestamp,
+                  position = 0.0, velocity = 0.0):
+        self.time = timestamp
+        self.state[0,0] = position
+        self.state[1,0] = velocity
+        self.state_cov = numpy.identity(2) * default_var
+
+    def A_matrix(self, dt=default_dt):
+        A = numpy.identity(2)
+        A[0,1] = dt
+        return A
+
+    def B_matrix(self, dt=default_dt):
+        B = numpy.zeros((2,1))
+        B[0,0] = 0.5*dt**2
+        B[1,0] = dt
+        return B
+
+    def C_matrix(self):
+        C = numpy.zeros((1, 2))
+        C[0,0] = 1
+        return C
+
+    def get_state(self):
+        return self.state, self.state_cov
+
+    def get_estimate(self):
+        z = self.C_matrix() @ self.state
+        z_cov = self.C_matrix() @ self.state_cov @ self.C_matrix().transpose()
+        return z, z_cov
+
+    def get_input(self, var=default_var):
+        u_nominal = self.T / self.m - self.g
+        u_var = var / self.m**2
+        u = u_nominal + numpy.random.normal(0, math.sqrt(u_var))
+        return u
+
+    def peek_pos(self):
+        return self.state[0, 0]
+
+    def peek_vel(self):
+        return self.state[1, 0]
+
+    def _predict(self, timestamp, input, input_var=default_var):
+        if self.time is None:
+            raise RuntimeError('unintialized filter')
+        delta_t = timestamp - self.time
+        self.time = timestamp
+
+        u = numpy.array([[input / self.m - self.g]])
+        sigma_u = numpy.array([[input_var / self.m**2]])
+
+        self.predicted_state = self.A_matrix(delta_t) @ \
+            self.state + self.B_matrix(delta_t) @ u
+        self.predicted_state_cov = \
+            self.A_matrix(delta_t) @ self.state_cov @ \
+            self.A_matrix(delta_t).transpose() + \
+            self.B_matrix(delta_t) @ sigma_u @ \
+            self.B_matrix(delta_t).transpose()
+
+    def _skip_measure(self):
+        self.state = self.predicted_state
+        self.state_cov = numpy.identity(2) * default_var
+
+    def _measure(self, measurement, measurement_var = default_var):
+        sigma_z = numpy.array([[measurement_var]])
+        z = numpy.array([[measurement]])
+        SigmaXCT = self.predicted_state_cov @ self.C_matrix().transpose()
+        CSigmaXCT = self.C_matrix() @ SigmaXCT
+        K = SigmaXCT @ numpy.linalg.inv(CSigmaXCT + sigma_z)
+        self.state = self.predicted_state + K @ \
+            (z - self.C_matrix() @ self.predicted_state)
+        self.state_cov = (numpy.identity(2) - K @ self.C_matrix()) @ \
+            self.predicted_state_cov
+
+    def advance_filter(self, timestamp, input, measurement, input_var, measurement_var):
+        self._predict(timestamp, input, input_var)
+        self._measure(measurement, measurement_var)
+
+    def simulate_system(self, timestamp, input, input_var):
+        self._predict(timestamp, input, input_var)
+        self._skip_measure()
---
+++ hw/HW3/simulate_drone_kf.py
@@ -0,0 +1,94 @@
+from drone_kf import DroneKF
+import matplotlib.pyplot as plt
+import numpy
+import math
+import random
+
+# Drone parameters
+m = 0.25
+T = 2.7
+g = 9.81
+time_step = 1 / 200
+
+bot_model = DroneKF(m, T)
+bot_model.set_state(0)
+
+estimator = DroneKF(m, T)
+estimator.set_state(0)
+
+mismatched_estimator = DroneKF(1.1*m, T)
+mismatched_estimator.set_state(0)
+
+thrust_var = 0.25
+
+# 5 seconds of simulation
+t = time_step
+all_t = [i * t for i in range(1, 1001)]
+
+# u_nominal = [T / m - g for time in all_t]
+# var_u = thrust_var / m**2
+# input = [u+n for u, n in zip(u_nominal, list(numpy.random.normal(0, math.sqrt(var_u),len(all_t))))]
+
+noisy_thrust = [T + numpy.random.normal(0, math.sqrt(thrust_var)) for i in all_t]
+
+min_var_z = 0.01
+max_var_z = 0.5
+
+h_ground_truth = []
+h_sensor = []
+h_estimate = []
+h_mismatched_estimate = []
+
+def generate_measurement(hm):
+    var_zh = random.uniform(min_var_z, max_var_z)
+    zh = numpy.random.normal(hm, math.sqrt(var_zh))
+    return zh, var_zh
+
+def calculate_error(gt, diff):
+    error = [x-y for x, y in zip(gt, diff)]
+    return error
+
+for i in range(len(all_t)):
+
+    input = noisy_thrust[i]
+    bot_model.simulate_system(all_t[i], input, thrust_var)
+    hgt = bot_model.peek_pos()
+    h_ground_truth.append(hgt)
+
+    zh, var_zh = generate_measurement(hgt)
+    h_sensor.append(zh)
+
+    estimator.advance_filter(all_t[i], input, zh, thrust_var, var_zh)
+    h_est, h_est_cov = estimator.get_estimate()
+    h_estimate.append(h_est[0])
+
+    mismatched_estimator.advance_filter(all_t[i], input, zh, thrust_var, var_zh)
+    h_mismatched_est, h_mismatched_est_cov = mismatched_estimator.get_estimate()
+    h_mismatched_estimate.append(h_mismatched_est[0])
+
+plt.figure(1)
+plt.title('Drone Altitude in 4 Different Scenarios')
+plt.plot(all_t, h_ground_truth, c='k', linewidth=3, label='Ground Truth Altitude')
+plt.plot(all_t, h_sensor, c='y', alpha=0.3, label='Sensor Altitude')
+plt.plot(all_t, h_estimate, c='r', linewidth=2, linestyle='dashed', label='Estimated Altitude')
+plt.plot(all_t, h_mismatched_estimate, c='b', linewidth=2, linestyle='dashed', label='Model Mismatch Estimated Altitude')
+plt.ylabel('Altitude H [m]')
+plt.xlabel('Time (s)')
+plt.grid()
+plt.legend()
+
+sensor_error = calculate_error(h_ground_truth, h_sensor)
+estimate_error = calculate_error(h_ground_truth, h_estimate)
+mismatched_estimate_error = calculate_error(h_ground_truth, h_mismatched_estimate)
+
+plt.figure(2)
+plt.title('Drone Altitude Error from Simulation Ground Truth')
+plt.plot(all_t, sensor_error, c='y', alpha=0.3, label='Sensor Measurement Error')
+plt.plot(all_t, estimate_error, c='r', label='Kalman Filter Estimation Error')
+plt.plot(all_t, mismatched_estimate_error, c='b', label='Mismatched Model KF Estimation Error')
+plt.ylabel('Altitude Error [m]')
+plt.xlabel('Time (s)')
+plt.grid()
+plt.legend()
+
+plt.show()
